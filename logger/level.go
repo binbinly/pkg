19a27ca6f7c51b1fa34b094442a4bae60c93eb22
binbinly <809870131@qq.com>
2023-05-17 12:01:53 +0800

    edit

--- logger/level.go
+++ logger/level.go
@@ -6,13 +6,22 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+const (
+	DebugLevel = "debug"
+	InfoLevel  = "info"
+	WarnLevel  = "warn"
+	ErrorLevel = "error"
+	PanicLevel = "panic"
+	FatalLevel = "fatal"
+)
+
 var loggerLevels = map[string]zapcore.Level{
-	"debug": zapcore.DebugLevel,
-	"info":  zapcore.InfoLevel,
-	"warn":  zapcore.WarnLevel,
-	"error": zapcore.ErrorLevel,
-	"panic": zapcore.PanicLevel,
-	"fatal": zapcore.FatalLevel,
+	DebugLevel: zapcore.DebugLevel,
+	InfoLevel:  zapcore.InfoLevel,
+	WarnLevel:  zapcore.WarnLevel,
+	ErrorLevel: zapcore.ErrorLevel,
+	PanicLevel: zapcore.PanicLevel,
+	FatalLevel: zapcore.FatalLevel,
 }
 
 func getZapLevel(l string) zapcore.Level {
@@ -25,43 +34,43 @@ func getZapLevel(l string) zapcore.Level {
 }
 
 func Debug(args ...interface{}) {
-	logger.Log("debug", args...)
+	logger.Log(DebugLevel, args...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	logger.Logf("debug", format, args...)
+	logger.Logf(DebugLevel, format, args...)
 }
 
 func Info(args ...interface{}) {
-	logger.Log("info", args...)
+	logger.Log(InfoLevel, args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	logger.Logf("info", format, args...)
+	logger.Logf(InfoLevel, format, args...)
 }
 
 func Warn(args ...interface{}) {
-	logger.Log("warn", args...)
+	logger.Log(WarnLevel, args...)
 }
 
 func Warnf(format string, args ...interface{}) {
-	logger.Logf("warn", format, args...)
+	logger.Logf(WarnLevel, format, args...)
 }
 
 func Error(args ...interface{}) {
-	logger.Log("error", args...)
+	logger.Log(ErrorLevel, args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	logger.Logf("error", format, args...)
+	logger.Logf(ErrorLevel, format, args...)
 }
 
 func Fatal(args ...interface{}) {
-	logger.Log("fatal", args...)
+	logger.Log(FatalLevel, args...)
 	os.Exit(1)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	logger.Logf("fatal", format, args...)
+	logger.Logf(FatalLevel, format, args...)
 	os.Exit(1)
 }
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -5,11 +5,11 @@ import (
 )
 
 func TestLogger(t *testing.T) {
-	Log("info", "test info")
-	Logf("info", "test info %d", 1)
+	Log(InfoLevel, "test info")
+	Logf(InfoLevel, "test info %d", 1)
 
-	Fields(map[string]any{"name": "app"}).Log("error", "test error")
-	Fields(map[string]any{"name": "app"}).Logf("error", "test error %d", 1)
+	Fields(map[string]any{"name": "app"}).Log(ErrorLevel, "test error")
+	Fields(map[string]any{"name": "app"}).Logf(ErrorLevel, "test error %d", 1)
 
 	Debug("test error")
 	Debugf("test error %d", 1)
